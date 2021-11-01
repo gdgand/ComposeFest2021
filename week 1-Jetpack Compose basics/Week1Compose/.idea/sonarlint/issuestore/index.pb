1f3a30e56c1a6497a81c7563881680471c3b6231
Jeong Hyeon Seok <peebbv6364@gmail.com>
2021-11-01 23:23:14 +0900

    Week1 : Jetpack Compose basics CodeLab Complete

---
+++ week 1-Jetpack Compose basics/Week1Compose/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+e
+5app/src/main/java/kr/hs/week1compose/ui/theme/Type.kt,c\a\ca7f066a069397d436de35399b97337f922ceb52
+f
+6app/src/main/java/kr/hs/week1compose/ui/theme/Color.kt,4\c\4ccdb576262a5f0ffd7a07872a317ef9e1986612
+f
+6app/src/main/java/kr/hs/week1compose/ui/theme/Shape.kt,6\4\64a49fb593e75bfd157770c6164a79aa3f06fc56
+f
+6app/src/main/java/kr/hs/week1compose/ui/theme/Theme.kt,7\5\75c71d7bb715b85ddddb266e456eddaf6a996314
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+d
+4app/src/main/java/kr/hs/week1compose/MainActivity.kt,a\b\abb6df53da5d37ed53cdc721ee3a4f1a2d44e49a
--- week 1-Jetpack Compose basics/Week1Compose/app/src/main/java/kr/hs/week1compose/MainActivity.kt
+++ week 1-Jetpack Compose basics/Week1Compose/app/src/main/java/kr/hs/week1compose/MainActivity.kt
@@ -1,13 +1,40 @@
 package kr.hs.week1compose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kr.hs.week1compose.ui.theme.Week1ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +42,102 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Week1ComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun MyApp() {
+    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
+
+    if (shouldShowOnBoarding) {
+        OnBoardingScreen { shouldShowOnBoarding = false }
+    } else {
+        Greetings()
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    val expanded = rememberSaveable { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(PaddingValues(vertical = 4.dp, horizontal = 8.dp))
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+            ) {
+                Text(text = "Hello")
+                Text(
+                    text = name, style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
+            }
+
+            IconButton(onClick = { expanded.value = expanded.value.not() }) {
+                when (expanded.value) {
+                    true -> Icon(Icons.Filled.ExpandLess, stringResource(R.string.show_less))
+                    false -> Icon(Icons.Filled.ExpandMore, stringResource(R.string.show_more))
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES, name = "DefaultPreviewDark")
 @Composable
 fun DefaultPreview() {
     Week1ComposeTheme {
-        Greeting("Android")
+        Greetings()
+    }
+}
+
+@Composable
+fun OnBoardingScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = { onContinueClicked() }
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    Week1ComposeTheme {
+        OnBoardingScreen { /** Nothing To Do **/ }
     }
 }
