0def0c962b52b5be81491fdd5174751f40b7aa2f
myeongki.cho <myeongki.cho@navercorp.com>
2021-11-02 00:09:01 +0900

    Typography에 button, caption 설정

--- week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/codelab/basics/ui/Type.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/codelab/basics/ui/Type.kt
@@ -28,8 +28,7 @@ val typography = Typography(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
-    )
-    /* Other default text styles to override
+    ),
     button = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.W500,
@@ -40,5 +39,4 @@ val typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
     )
-    */
 )
