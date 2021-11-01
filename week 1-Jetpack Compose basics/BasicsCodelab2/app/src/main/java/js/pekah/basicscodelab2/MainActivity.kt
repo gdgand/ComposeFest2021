129e6a93375ff255a1dba18e7e99341639590acb
JosephNaa <js.pekah@gmail.com>
2021-11-01 23:33:33 +0900

    4. Tweaking the UI

--- week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/js/pekah/basicscodelab2/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/js/pekah/basicscodelab2/MainActivity.kt
@@ -3,11 +3,14 @@ package js.pekah.basicscodelab2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import js.pekah.basicscodelab2.ui.theme.BasicsCodelab2Theme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +29,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
 }
 
 @Preview(showBackground = true, name = "Text preview")
