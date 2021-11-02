e1be36d6ec9f954e86727211a88b0d2c4cf65f91
Devinear <leech8757@gmail.com>
2021-11-02 23:11:27 +0900

    Empty Compose Project 및 Init Setting

---
+++ week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/codelab/basicscodelab2/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.codelab.basicscodelab2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.codelab.basicscodelab2.ui.theme.BasicsCodelab2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicsCodelab2Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BasicsCodelab2Theme {
+        Greeting("Android")
+    }
+}
