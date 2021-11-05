729bed298ca32b72c7d48439b02d4f4fe646a776
hyejineee <dev.hyejin04@gmail.com>
2021-11-05 17:59:27 +0900

    컴포즈 베이직 코드랩 12까지 완료

--- week 1-Jetpack Compose basics/HyejineeeBasicsCodelab/app/src/main/java/com/example/hyejineeebasicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/HyejineeeBasicsCodelab/app/src/main/java/com/example/hyejineeebasicscodelab/MainActivity.kt
@@ -1,21 +1,21 @@
 package com.example.hyejineeebasicscodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.*
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.hyejineeebasicscodelab.ui.theme.HyejineeeBasicsCodelabTheme
@@ -32,9 +32,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun Greetings(names: List<String>) {
+    LazyColumn(
+        modifier = Modifier.padding(vertical = 20.dp)
+    ) {
+        items(items = names){ name->
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    val expended = remember{ mutableStateOf(false)}
-    val extraPadding = if(expended.value) 48.dp else 0.dp
+
+    var expended by rememberSaveable { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if (expended) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
+    val handleClick = {
+        expended = !expended
+    }
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -43,14 +65,18 @@ fun Greeting(name: String) {
 
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
-                modifier = Modifier.weight(1f).padding(bottom = extraPadding)
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
-                Text(text = "$name")
+                Text(text = "$name", style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
             }
 
-            OutlinedButton(onClick = {expended.value = !expended.value}){
-                Text(text = if(expended.value) "Show Less" else "Show More")
+            OutlinedButton(onClick = handleClick) {
+                Text(text = if (expended) "Show Less" else "Show More")
             }
 
         }
@@ -59,22 +85,62 @@ fun Greeting(name: String) {
 }
 
 @Composable
-fun MyApp(names :List<String> = listOf("World", "Compose")) {
+fun MyApp(names: List<String> = List<String>(1000){"$it"}) {
+    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
+    val handleContinueClick = {
+        shouldShowOnBoarding = false
+    }
     Surface(color = MaterialTheme.colors.background) {
-        Column(
-            modifier = Modifier.padding(4.dp)
-        ) {
-            for(name in names){
-                Greeting(name = name)
-            }
+        if(shouldShowOnBoarding){
+            OnBoardingScreen(handleContinueClick = handleContinueClick)
+        }else{
+            Greetings(names = names)
         }
 
+    }
+}
+
+@Composable
+fun OnBoardingScreen(handleContinueClick :()->Unit) {
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Welcome to the basics Codelab!")
+            Button(
+                onClick = handleContinueClick,
+                modifier = Modifier.padding(24.dp),
+            ) {
+                Text(text = "Continue")
+            }
+
+        }
+    }
+}
+
+
+@Preview(showBackground = true, name = "onBoarding", widthDp = 320, heightDp = 320)
+@Composable
+fun onBoardingPreview() {
+    HyejineeeBasicsCodelabTheme {
+        OnBoardingScreen({})
     }
 }
 
 @Preview(showBackground = true, name = "this is preview", widthDp = 320)
 @Composable
 fun DefaultPreview() {
+    HyejineeeBasicsCodelabTheme {
+        MyApp()
+    }
+}
+
+@Preview(showBackground = true, name = "Dark Mode Preview", widthDp = 320, uiMode = UI_MODE_NIGHT_YES )
+@Composable
+fun DefaultPreviewDarkMode(){
     HyejineeeBasicsCodelabTheme {
         MyApp()
     }
