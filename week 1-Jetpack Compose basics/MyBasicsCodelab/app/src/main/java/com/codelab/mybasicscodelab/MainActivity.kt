6e545a16f7190e657e88505134e02d60188f0007
Wanjuuuuu <makarong5798@gmail.com>
2021-11-07 17:13:16 +0900

    Make pretty with changing theme colors

--- week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/codelab/mybasicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/codelab/mybasicscodelab/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.codelab.mybasicscodelab
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.mybasicscodelab.ui.theme.MyBasicsCodelabTheme
@@ -72,7 +74,10 @@ fun Greetings(names: List<String> = List(1000) { "$it" }) {
     }
 }
 
-@Preview(showBackground = true, name = "Text Preview", widthDp = 320)
+@Preview(
+    showBackground = true, name = "Text Preview", widthDp = 320,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
+)
 @Composable
 fun GreetingsPreview() {
     MyBasicsCodelabTheme {
@@ -107,7 +112,12 @@ fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello ")
-                Text(text = "$name!")
+                Text(
+                    text = "$name!", style = MaterialTheme.typography.h4.copy(
+                        fontWeight =
+                        FontWeight.Bold
+                    )
+                )
             }
             OutlinedButton(onClick = { expanded.value = !expanded.value }) {
                 Text(text = if (expanded.value) "Show less" else "Show more")
--- week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/codelab/mybasicscodelab/ui/theme/Theme.kt
+++ week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/codelab/mybasicscodelab/ui/theme/Theme.kt
@@ -5,17 +5,20 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Navy,
+    onPrimary = Chartreuse,
+    surface = Blue,
+    onSurface = Navy
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = LightBlue,
+    onPrimary = Navy,
+    surface = Blue,
+    onSurface = Color.White
 
     /* Other default colors to override
     background = Color.White,
