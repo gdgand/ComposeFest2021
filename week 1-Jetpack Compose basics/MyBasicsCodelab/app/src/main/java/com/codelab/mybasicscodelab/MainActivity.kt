04eae5ed8b124e1b37890a2adcea202c78199529
Wanjuuuuu <makarong5798@gmail.com>
2021-11-07 16:07:19 +0900

    Apply Column and Row to lay out Components

--- week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/codelab/mybasicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/codelab/mybasicscodelab/MainActivity.kt
@@ -3,8 +3,12 @@ package com.codelab.mybasicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -18,32 +22,50 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyBasicsCodelabTheme {
-                myApp()
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun myApp() {
+fun MyApp(names: List<String> = listOf("World", "Compose")) {
     // A surface container using the 'background' color from the theme
-    Surface(color = MaterialTheme.colors.background) {
-        Greeting("Android")
+    Surface(color = MaterialTheme.colors.background, modifier = Modifier.padding(4.dp)) {
+        Column {
+            for (name in names) {
+                Greeting(name = name)
+            }
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
+    Surface(
+        color = MaterialTheme.colors.primary, modifier = Modifier.padding(
+            vertical = 4.dp,
+            horizontal = 8.dp
+        )
+    ) {
         // use text on top of it as onPrimary
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+        Row(modifier = Modifier.padding(24.dp)) {
+            // weight fills all available space -> removed fillMaxWidth
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello ")
+                Text(text = "$name!")
+            }
+            OutlinedButton(onClick = { /*TODO*/ }) {
+                Text(text = "Show more")
+            }
+        }
     }
 }
 
-@Preview(showBackground = true, name = "Text Preview")
+@Preview(showBackground = true, name = "Text Preview", widthDp = 320)
 @Composable
 fun DefaultPreview() {
     MyBasicsCodelabTheme {
-        myApp()
+        MyApp()
     }
 }
