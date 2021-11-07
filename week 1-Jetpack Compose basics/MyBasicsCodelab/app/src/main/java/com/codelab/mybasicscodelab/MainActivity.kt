c47cb66b0e68e72329c058729a62faa44908cd93
Wanjuuuuu <makarong5798@gmail.com>
2021-11-07 16:45:51 +0900

    Try hoising state which elevates state value to share same state

--- week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/codelab/mybasicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/codelab/mybasicscodelab/MainActivity.kt
@@ -3,17 +3,10 @@ package com.codelab.mybasicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,14 +24,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(names: List<String> = listOf("World", "Compose")) {
-    // A surface container using the 'background' color from the theme
-    Surface(color = MaterialTheme.colors.background, modifier = Modifier.padding(4.dp)) {
-        Column {
-            for (name in names) {
-                Greeting(name = name)
+fun MyApp() {
+    var shouldShowOnBoarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnBoarding) {
+        OnBoardingScreen(onContinueClicked = { shouldShowOnBoarding = false })
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun OnBoardingScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(modifier = Modifier.padding(24.dp), onClick = onContinueClicked) {
+                Text(text = "Continue")
             }
         }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnBoardingPreview() {
+    MyBasicsCodelabTheme {
+        OnBoardingScreen(onContinueClicked = {})
+    }
+}
+
+@Composable
+fun Greetings(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Preview(showBackground = true, name = "Text Preview", widthDp = 320)
+@Composable
+fun GrettingsPreview() {
+    MyBasicsCodelabTheme {
+        Greetings()
     }
 }
 
@@ -69,13 +102,5 @@ fun Greeting(name: String) {
                 Text(text = if (expanded.value) "Show less" else "Show more")
             }
         }
-    }
-}
-
-@Preview(showBackground = true, name = "Text Preview", widthDp = 320)
-@Composable
-fun DefaultPreview() {
-    MyBasicsCodelabTheme {
-        MyApp()
     }
 }
