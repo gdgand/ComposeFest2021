2e5dfb83a58ea75fd6ed746efe49d00798a96666
Wanjuuuuu <makarong5798@gmail.com>
2021-11-07 15:53:14 +0900

    Apply Text backgroud color, padding and reuse composable function

--- week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/codelab/mybasicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/codelab/mybasicscodelab/MainActivity.kt
@@ -3,11 +3,14 @@ package com.codelab.mybasicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codelab.mybasicscodelab.ui.theme.MyBasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +18,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyBasicsCodelabTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                myApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun myApp() {
+    // A surface container using the 'background' color from the theme
+    Surface(color = MaterialTheme.colors.background) {
+        Greeting("Android")
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colors.primary) {
+        // use text on top of it as onPrimary
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
+}
+
+@Preview(showBackground = true, name = "Text Preview")
 @Composable
 fun DefaultPreview() {
     MyBasicsCodelabTheme {
-        Greeting("Android")
+        myApp()
     }
 }
