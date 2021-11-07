ea35ab9d2ce8c84a20f49fd3ba179b260d62de65
Wanjuuuuu <makarong5798@gmail.com>
2021-11-07 16:17:26 +0900

    Apply mutable state to preserve previous value and then change with it

--- week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/codelab/mybasicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/MyBasicsCodelab/app/src/main/java/com/codelab/mybasicscodelab/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,6 +44,10 @@ fun MyApp(names: List<String> = listOf("World", "Compose")) {
 
 @Composable
 fun Greeting(name: String) {
+    // if expanded state is changed, will be recomposed
+    // btw state won't be preserved when config changed
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colors.primary, modifier = Modifier.padding(
             vertical = 4.dp,
@@ -51,12 +57,16 @@ fun Greeting(name: String) {
         // use text on top of it as onPrimary
         Row(modifier = Modifier.padding(24.dp)) {
             // weight fills all available space -> removed fillMaxWidth
-            Column(modifier = Modifier.weight(1f)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello ")
                 Text(text = "$name!")
             }
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show more")
+            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(text = if (expanded.value) "Show less" else "Show more")
             }
         }
     }
