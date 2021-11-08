49c5e7e46f8e6b0b743c830f5803c558dc22c052
hyunjung-choi <hjchoi.dev@gmail.com>
2021-11-08 20:36:23 +0900

    week 1 - Jetpack Compose basics 코드랩 완료

--- week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/hyunjung/basicscodelab2/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/hyunjung/basicscodelab2/MainActivity.kt
@@ -1,16 +1,25 @@
 package com.hyunjung.basicscodelab2
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hyunjung.basicscodelab2.ui.theme.BasicsCodelab2Theme
@@ -39,7 +48,6 @@ fun MyApp() {
 
 @Composable
 fun OnboardingScreen(onContinueClicked: () -> Unit) {
-
     Surface {
         Column(
             modifier = Modifier.fillMaxSize(),
@@ -67,7 +75,7 @@ private fun Greetings(names: List<String> = List(1000) { "$it" }) {
     }
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun OnboardingPreview() {
     BasicsCodelab2Theme {
@@ -77,10 +85,15 @@ fun OnboardingPreview() {
 
 @Composable
 fun Greeting(name: String) {
+    var expanded by remember { mutableStateOf(false) }
 
-    val expanded = remember { mutableStateOf(false) }
-
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -89,20 +102,35 @@ fun Greeting(name: String) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
                 .weight(1f)
-                .padding(bottom = extraPadding)) {
-                Text(text = "Hello,")
-                Text(text = name)
-            }
-            OutlinedButton(
-                onClick = { expanded.value = !expanded.value }
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+                Text(text = "Hello, ")
+                Text(text = name, style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold))
+                if (expanded) {
+                    Text (text = ("Composem ipsum color sit lazy, " + "padding theme elit, sed do bouncy").repeat(4))
+                }
+            }
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon (
+                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription =
+                    if (expanded) stringResource(id = R.string.show_less)
+                    else stringResource(id = R.string.show_less)
+                    )
             }
         }
     }
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+// Preview Dark Mode
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicsCodelab2Theme {
--- week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/hyunjung/basicscodelab2/ui/theme/Theme.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/com/hyunjung/basicscodelab2/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -13,7 +14,10 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy,
     primaryVariant = Purple700,
     secondary = Teal200
 
