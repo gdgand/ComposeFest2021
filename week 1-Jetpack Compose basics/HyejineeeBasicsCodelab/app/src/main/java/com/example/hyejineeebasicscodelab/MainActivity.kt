fb65b183b1d666dbf59d2a148e80b93f87624f46
hyejineee <dev.hyejin04@gmail.com>
2021-11-08 19:14:11 +0900

    jetpack compose 스터디 - 1주차 완료

--- week 1-Jetpack Compose basics/HyejineeeBasicsCodelab/app/src/main/java/com/example/hyejineeebasicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/HyejineeeBasicsCodelab/app/src/main/java/com/example/hyejineeebasicscodelab/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -11,10 +12,14 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,64 +41,93 @@ fun Greetings(names: List<String>) {
     LazyColumn(
         modifier = Modifier.padding(vertical = 20.dp)
     ) {
-        items(items = names){ name->
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
+fun Greeting(name: String, expanded:Boolean = false) {
 
-    var expended by rememberSaveable { mutableStateOf(false) }
-    val extraPadding by animateDpAsState(
-        if (expended) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
+    var expanded by rememberSaveable { mutableStateOf(expanded) }
 
-    val handleClick = {
-        expended = !expended
+    Card(
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardGreeting(name = name, expanded = expanded)
     }
 
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 4.dp)
+
+}
+
+@Composable
+fun CardGreeting(name:String, expanded: Boolean){
+    var expanded by rememberSaveable{ mutableStateOf(expanded)}
+
+    Row(
+        modifier = Modifier
+            .padding(24.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
     ) {
-
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello,")
-                Text(text = "$name", style = MaterialTheme.typography.h4.copy(
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello,")
+            Text(
+                text = "$name", style = MaterialTheme.typography.h4.copy(
                     fontWeight = FontWeight.ExtraBold
-                ))
-            }
+                )
+            )
 
-            OutlinedButton(onClick = handleClick) {
-                Text(text = if (expended) "Show Less" else "Show More")
+            if(expanded){
+                Text(text = ("Composem ipsum color sit lazy, " +
+                        "padding theme elit, sed do bouncy. ").repeat(4),
+                    modifier = Modifier.padding(10.dp)
+                )
             }
+        }
+
+        IconButton(
+            onClick = { expanded = !expanded },
+        ) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                }
+
+            )
 
         }
 
     }
+
 }
 
+
 @Composable
-fun MyApp(names: List<String> = List<String>(1000){"$it"}) {
-    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(true) }
+fun MyApp(
+    names: List<String> = List<String>(1000) { "$it" },
+    shouldShowOnBoarding: Boolean = true
+) {
+    var shouldShowOnBoarding by rememberSaveable { mutableStateOf(shouldShowOnBoarding) }
     val handleContinueClick = {
         shouldShowOnBoarding = false
     }
     Surface(color = MaterialTheme.colors.background) {
-        if(shouldShowOnBoarding){
+        if (shouldShowOnBoarding) {
             OnBoardingScreen(handleContinueClick = handleContinueClick)
-        }else{
+        } else {
             Greetings(names = names)
         }
 
@@ -101,11 +135,13 @@ fun MyApp(names: List<String> = List<String>(1000){"$it"}) {
 }
 
 @Composable
-fun OnBoardingScreen(handleContinueClick :()->Unit) {
+fun OnBoardingScreen(handleContinueClick: () -> Unit) {
 
     Surface {
         Column(
-            modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -118,6 +154,22 @@ fun OnBoardingScreen(handleContinueClick :()->Unit) {
             }
 
         }
+    }
+}
+
+@Preview(showBackground = true, name="ExpandedGreetingPreview", widthDp = 320, heightDp = 320)
+@Composable
+fun ExpandedGreetingPreview(){
+    HyejineeeBasicsCodelabTheme() {
+        Greeting(name = "hyejineee", true)
+    }
+}
+
+@Preview(showBackground = true, name="DefaultGreetingPreview", widthDp = 320, heightDp = 150)
+@Composable
+fun DefaultGreetingPreview(){
+    HyejineeeBasicsCodelabTheme() {
+        Greeting(name = "hyejineee", false)
     }
 }
 
@@ -134,14 +186,19 @@ fun onBoardingPreview() {
 @Composable
 fun DefaultPreview() {
     HyejineeeBasicsCodelabTheme {
-        MyApp()
+        MyApp(shouldShowOnBoarding = false)
     }
 }
 
-@Preview(showBackground = true, name = "Dark Mode Preview", widthDp = 320, uiMode = UI_MODE_NIGHT_YES )
+@Preview(
+    showBackground = true,
+    name = "Dark Mode Preview",
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES
+)
 @Composable
-fun DefaultPreviewDarkMode(){
+fun DefaultPreviewDarkMode() {
     HyejineeeBasicsCodelabTheme {
-        MyApp()
+        MyApp(shouldShowOnBoarding = false)
     }
 }
