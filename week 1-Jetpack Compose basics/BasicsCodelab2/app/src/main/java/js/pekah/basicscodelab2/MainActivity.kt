6d13886516a3abe4b605f60cfe34dd101a225095
JosephNaa <js.pekah@gmail.com>
2021-11-08 20:20:09 +0900

    10. Peristing state

--- week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/js/pekah/basicscodelab2/MainActivity.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab2/app/src/main/java/js/pekah/basicscodelab2/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,7 +28,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun MyApp() {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
         OnboardingScreen(onContinueClicked = {shouldShowOnboarding = false})
