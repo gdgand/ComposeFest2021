c146d0b43ebee450ffe90b5367c5a897bf611b33
hello2727 <jihye7369@gmail.com>
2021-11-12 00:20:22 +0900

    코드랩 1주차 codelab 완료 - compose basics

--- week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/example/android/basicscodelab/ui/theme/Theme.kt
+++ week 1-Jetpack Compose basics/BasicsCodelab/app/src/main/java/com/example/android/basicscodelab/ui/theme/Theme.kt
@@ -5,17 +5,20 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
 
     /* Other default colors to override
     background = Color.White,
