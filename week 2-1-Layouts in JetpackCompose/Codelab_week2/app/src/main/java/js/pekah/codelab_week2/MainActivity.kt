6a3ddee93b0ec8ee309310fd9c007c1c26edd78a
JosephNaa <js.pekah@gmail.com>
2021-11-16 21:11:39 +0900

    9. Layout modifiers under the hood

--- week 2-1-Layouts in JetpackCompose/Codelab_week2/app/src/main/java/js/pekah/codelab_week2/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/Codelab_week2/app/src/main/java/js/pekah/codelab_week2/MainActivity.kt
@@ -14,17 +14,17 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.layout
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.*
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.*
 import coil.compose.rememberImagePainter
 import js.pekah.codelab_week2.ui.theme.Codelab_week2Theme
 import kotlinx.coroutines.launch
@@ -114,7 +114,13 @@ fun LayoutsCodelab() {
 
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
+    Row(
+        modifier = modifier
+            .background(color = Color.LightGray, shape = RectangleShape)
+            .padding(16.dp)
+            .size(200.dp)
+            .horizontalScroll(rememberScrollState())
+    ) {
         StaggeredGrid {
             for (topic in topics) {
                 Chip(modifier = Modifier.padding(8.dp), text = topic)
@@ -248,20 +254,14 @@ fun StaggeredGrid(
         content = content
     ) { measurables, constraints ->
 
-        // Keep track of the width of each row
         val rowWidths = IntArray(rows) { 0 }
 
-        // Keep track of the max height of each row
         val rowHeights = IntArray(rows) { 0 }
 
-        // Don't constrain child views further, measure them with given constraints
-        // List of measured children
         val placeables = measurables.mapIndexed { index, measurable ->
 
-            // Measure each child
             val placeable = measurable.measure(constraints)
 
-            // Track the width and max height of each row
             val row = index % rows
             rowWidths[row] += placeable.width
             rowHeights[row] = Math.max(rowHeights[row], placeable.height)
@@ -269,24 +269,18 @@ fun StaggeredGrid(
             placeable
         }
 
-        // Grid's width is the widest row
         val width = rowWidths.maxOrNull()
             ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
 
-        // Grid's height is the sum of the tallest element of each row
-        // coerced to the height constraints
         val height = rowHeights.sumOf { it }
             .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
 
-        // Y of each row, based on the height accumulation of previous rows
         val rowY = IntArray(rows) { 0 }
         for (i in 1 until rows) {
             rowY[i] = rowY[i-1] + rowHeights[i-1]
         }
 
-        // Set the size of the parent layout
         layout(width, height) {
-            // x cord we have placed up to, per row
             val rowX = IntArray(rows) { 0 }
 
             placeables.forEachIndexed { index, placeable ->
@@ -335,3 +329,39 @@ val topics = listOf(
     "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
     "Religion", "Social sciences", "Technology", "TV", "Writing"
 )
+
+@Stable
+fun Modifier.padding(all: Dp) =
+    this.then(
+        PaddingModifier(start = all, top = all, end = all, bottom = all, rtlAware = true)
+    )
+
+private class PaddingModifier(
+    val start: Dp = 0.dp,
+    val top: Dp = 0.dp,
+    val end: Dp = 0.dp,
+    val bottom: Dp = 0.dp,
+    val rtlAware: Boolean,
+) : LayoutModifier {
+
+    override fun MeasureScope.measure(
+        measurable: Measurable,
+        constraints: Constraints
+    ): MeasureResult {
+
+        val horizontal = start.roundToPx() + end.roundToPx()
+        val vertical = top.roundToPx() + bottom.roundToPx()
+
+        val placeable = measurable.measure(constraints.offset(-horizontal, -vertical))
+
+        val width = constraints.constrainWidth(placeable.width + horizontal)
+        val height = constraints.constrainHeight(placeable.height + vertical)
+        return layout(width, height) {
+            if (rtlAware) {
+                placeable.placeRelative(start.roundToPx(), top.roundToPx())
+            } else {
+                placeable.place(start.roundToPx(), top.roundToPx())
+            }
+        }
+    }
+}
