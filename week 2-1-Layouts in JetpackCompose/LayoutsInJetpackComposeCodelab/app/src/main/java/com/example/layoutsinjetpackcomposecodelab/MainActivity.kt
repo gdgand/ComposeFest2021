d7a2ae387b8825ca5c530de81d91624142ed68e8
hyejineee <dev.hyejin04@gmail.com>
2021-11-16 19:19:33 +0900

    [Layouts in compose] chapter8까지 진행

--- week 2-1-Layouts in JetpackCompose/LayoutsInJetpackComposeCodelab/app/src/main/java/com/example/layoutsinjetpackcomposecodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutsInJetpackComposeCodelab/app/src/main/java/com/example/layoutsinjetpackcomposecodelab/MainActivity.kt
@@ -3,11 +3,10 @@ package com.example.layoutsinjetpackcomposecodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -21,8 +20,14 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.example.layoutsinjetpackcomposecodelab.ui.theme.LayoutsInJetpackComposeCodelabTheme
@@ -33,11 +38,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutsInJetpackComposeCodelabTheme {
-                SimpleList()
+                // SimpleList()
+                BodyContent()
             }
         }
     }
 }
+
 
 @Composable
 fun LayoutsCodelab() {
@@ -66,12 +73,30 @@ fun LayoutsCodelab() {
     }
 }
 
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
+
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Layouts codelab")
+//    MyOwnColumn(modifier = modifier.padding(8.dp)) {
+//        Text("MyOwnColumn")
+//        Text("places items")
+//        Text("vertically.")
+//        Text("We've done it by hand!")
+//    }
+
+    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
+        StaggeredGrid(modifier, 5) {
+            for(topic in topics){
+                Chip(text = topic, modifier = Modifier.padding(4.dp))
+            }
+        }
     }
+
 }
 
 @Preview
@@ -127,9 +152,13 @@ fun SimpleList() {
     val coroutineScope = rememberCoroutineScope()
 
     Column() {
-        Row(modifier = Modifier.padding(6.dp).fillMaxWidth()) {
+        Row(
+            modifier = Modifier
+                .padding(6.dp)
+                .fillMaxWidth()
+        ) {
             Button(
-                onClick = { coroutineScope.launch { scrollState.animateScrollToItem(0) }},
+                onClick = { coroutineScope.launch { scrollState.animateScrollToItem(0) } },
                 modifier = Modifier.weight(1f)
             ) {
                 Text(text = "Scroll to the top")
@@ -137,12 +166,13 @@ fun SimpleList() {
 
             Spacer(modifier = Modifier.width(10.dp))
             Button(
-                onClick = { coroutineScope.launch { scrollState.animateScrollToItem(itemSize-1) }},
+                onClick = { coroutineScope.launch { scrollState.animateScrollToItem(itemSize - 1) } },
                 modifier = Modifier.weight(1f)
             ) {
                 Text(text = "Scroll to the bottom")
             }
         }
+
         LazyColumn(state = scrollState) {
             items(10000) {
 //            Surface(
@@ -196,5 +226,123 @@ fun SimpleListItem(index: Int) {
 fun SimpleListItemPreview() {
     LayoutsInJetpackComposeCodelabTheme {
         SimpleListItem(0)
+    }
+}
+
+fun Modifier.firstBaselineToTop(firstBaselineToTop: Dp) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints = constraints)
+
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    LayoutsInJetpackComposeCodelabTheme() {
+        Text(text = "Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    LayoutsInJetpackComposeCodelabTheme() {
+        Text(text = "Hi there!", Modifier.padding(top = 32.dp))
+    }
+}
+
+@Composable
+fun MyOwnColumn(modifier: Modifier = Modifier, content: @Composable () -> Unit) {
+    Layout(modifier = modifier, content = content) { measurables, constraints ->
+        val placeables = measurables.map {
+            it.measure(constraints)
+        }
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            var y = 0
+            placeables.forEach {
+                it.placeRelative(x = 0, y = y)
+                y += it.height
+            }
+        }
+    }
+}
+
+@Composable
+fun StaggeredGrid(modifier: Modifier = Modifier, rows: Int = 3, content: @Composable () -> Unit) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        val rowWidths = IntArray(rows) { 0 }
+        val rowHeights = IntArray(rows) { 0 }
+
+        val placeables = measurables.mapIndexed { index, measurable ->
+            val placeable = measurable.measure(constraints)
+            val row = index % rows
+            rowWidths[row] += placeable.width
+            rowHeights[row] = maxOf(rowHeights[row], placeable.height)
+            placeable
+        }
+
+        val width =
+            rowWidths.maxOrNull()?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth))
+                ?: constraints.minWidth
+        val height = rowHeights.sum().coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i - 1] + rowHeights[i - 1]
+        }
+
+        layout(width, height) {
+            val rowX = IntArray(rows) { 0 }
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+    }
+}
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(16.dp, 16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ChipPreview() {
+    LayoutsInJetpackComposeCodelabTheme() {
+        Chip(text = "Hi there")
     }
 }
