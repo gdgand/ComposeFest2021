53cfe5f169c9b0ee1c4d31614c4b9b426d5cca6a
JosephNaa <js.pekah@gmail.com>
2021-11-16 21:21:25 +0900

    11. Intrinsics

--- week 2-1-Layouts in JetpackCompose/Codelab_week2/app/src/main/java/js/pekah/codelab_week2/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/Codelab_week2/app/src/main/java/js/pekah/codelab_week2/MainActivity.kt
@@ -207,6 +207,38 @@ fun ConstraintLayoutContentPreview() {
 }
 
 @Composable
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+    Row(modifier = modifier.height(IntrinsicSize.Min)) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.Start),
+            text = text1
+        )
+
+        Divider(color = Color.Black, modifier = Modifier.fillMaxHeight().width(1.dp))
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 4.dp)
+                .wrapContentWidth(Alignment.End),
+            text = text2
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TwoTextsPreview() {
+    Codelab_week2Theme {
+        Surface {
+            TwoTexts(text1 = "Hi", text2 = "there")
+        }
+    }
+}
+
+@Composable
 fun SimpleList() {
     val scrollState = rememberScrollState()
 
