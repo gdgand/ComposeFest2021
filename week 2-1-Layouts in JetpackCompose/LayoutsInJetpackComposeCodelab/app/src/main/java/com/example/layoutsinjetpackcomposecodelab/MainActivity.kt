1fcdd93b35ec4ccbc340885e1513798886da2d7b
hyejineee <dev.hyejin04@gmail.com>
2021-11-16 20:20:45 +0900

    [Layouts in compose] chapter12까지 진행

--- week 2-1-Layouts in JetpackCompose/LayoutsInJetpackComposeCodelab/app/src/main/java/com/example/layoutsinjetpackcomposecodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutsInJetpackComposeCodelab/app/src/main/java/com/example/layoutsinjetpackcomposecodelab/MainActivity.kt
@@ -458,3 +458,38 @@ fun ConstraintLayoutContentPreview() {
         DecoupledConstraintLayout()
     }
 }
+
+@Composable
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+    Row(modifier = modifier.height(IntrinsicSize.Min)) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.Start),
+            text = text1
+        )
+
+        Divider(color = Color.Black, modifier = Modifier
+            .fillMaxHeight()
+            .width(1.dp))
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 4.dp)
+                .wrapContentWidth(Alignment.End),
+
+            text = text2
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TwoTextsPreview() {
+    LayoutsInJetpackComposeCodelabTheme() {
+        Surface {
+            TwoTexts(text1 = "Hi", text2 = "there")
+        }
+    }
+}
