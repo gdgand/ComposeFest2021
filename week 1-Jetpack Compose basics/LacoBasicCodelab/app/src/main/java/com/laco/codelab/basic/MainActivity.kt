346576d1469eea5b4b6a930c7894cb334e37a868
laco-dev <laco2951@gmail.com>
2021-11-19 14:21:42 +0900

    Styling and theming your app

--- week 1-Jetpack Compose basics/LacoBasicCodelab/app/src/main/java/com/laco/codelab/basic/MainActivity.kt
+++ week 1-Jetpack Compose basics/LacoBasicCodelab/app/src/main/java/com/laco/codelab/basic/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.laco.codelab.basic
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -26,6 +27,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.laco.codelab.basic.ui.theme.LacoBasicCodelabTheme
@@ -108,7 +110,11 @@ fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(
+                    text = name, style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             OutlinedButton(onClick = { expanded = expanded.not() }) {
                 Text(if (expanded) "Show less" else "Show more")
@@ -117,7 +123,12 @@ fun Greeting(name: String) {
     }
 }
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
 @Composable
 fun DefaultPreview() {
     LacoBasicCodelabTheme {
