23b7519d23d9798e1474c47d5bfc6128ee902531
laco-dev <laco2951@gmail.com>
2021-11-19 14:19:36 +0900

    Animating your list

--- week 1-Jetpack Compose basics/LacoBasicCodelab/app/src/main/java/com/laco/codelab/basic/MainActivity.kt
+++ week 1-Jetpack Compose basics/LacoBasicCodelab/app/src/main/java/com/laco/codelab/basic/MainActivity.kt
@@ -3,6 +3,9 @@ package com.laco.codelab.basic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -85,8 +88,14 @@ fun OnboardingPreview() {
 
 @Composable
 fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -96,13 +105,13 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
-            OutlinedButton(onClick = { expanded.value = expanded.value.not() }) {
-                Text(if (expanded.value) "Show less" else "Show more")
+            OutlinedButton(onClick = { expanded = expanded.not() }) {
+                Text(if (expanded) "Show less" else "Show more")
             }
         }
     }
