c97afceeedc3750186908f94a79200e6fa28978c
hyejineee <dev.hyejin04@gmail.com>
2021-11-19 17:51:58 +0900

    [Jetpack Compose Theming] chapter9까지 진행 완료

---
+++ week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/theme/shape.kt
@@ -0,0 +1,12 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val JetnewsShapes = Shapes(
+    small = CutCornerShape(topStart = 8.dp),
+    medium = CutCornerShape(topStart = 24.dp),
+    large = RoundedCornerShape(8.dp)
+)
