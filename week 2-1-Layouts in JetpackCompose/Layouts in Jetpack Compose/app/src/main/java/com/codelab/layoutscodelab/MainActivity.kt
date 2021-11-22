b638b39d96984b8cdff59d194f653a1c6ec2d11b
Devinear <leech8757@gmail.com>
2021-11-22 21:26:18 +0900

    10 - Constraint Layout

--- week 2-1-Layouts in JetpackCompose/Layouts in Jetpack Compose/app/src/main/java/com/codelab/layoutscodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/Layouts in Jetpack Compose/app/src/main/java/com/codelab/layoutscodelab/MainActivity.kt
@@ -161,3 +161,35 @@ fun LayoutsCodelabPreview() {
         LayoutsCodelab()
     }
 }
+
+@Composable
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+    Row(modifier = modifier.height(IntrinsicSize.Min)) {
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 4.dp)
+                .wrapContentWidth(Alignment.Start),
+            text = text1
+        )
+
+        Divider(color = Color.Black, modifier = Modifier.fillMaxHeight().width(1.dp))
+        Text(
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 4.dp)
+                .wrapContentWidth(Alignment.End),
+            text = text2
+        )
+    }
+}
+
+@Preview
+@Composable
+fun TwoTextsPreview() {
+    LayoutsCodelabTheme {
+        Surface {
+            TwoTexts(text1 = "Hi", text2 = "there")
+        }
+    }
+}
