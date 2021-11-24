4ddf457a1fbde2eee1f9bfd7a7a517455aaa083a
hyejineee <dev.hyejin04@gmail.com>
2021-11-24 15:49:13 +0900

    [Test] TopAppBar tab click에 대한 테스트 작성
    TopAppBar의 다른 tab을 클릭시 탭이 잘 변경되는지 확인하기 위해 테스트를 작성했습니다.

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,10 +1,16 @@
 package com.example.compose.rally
 
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.text.toUpperCase
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import com.example.compose.rally.ui.theme.RallyTheme
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -12,25 +18,47 @@ class TopAppBarTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    @Test
-    fun rallyTopAppBarTest_currentLabelExists() {
+    @Before
+    fun setUpTopAppBar() {
         val allScreens = RallyScreen.values().toList()
-
         composeTestRule.setContent {
             RallyTheme {
+                var currentScreen by rememberSaveable { mutableStateOf(RallyScreen.Accounts) }
                 RallyTopAppBar(
                     allScreens = allScreens,
-                    onTabSelected = {},
-                    currentScreen = RallyScreen.Accounts
+                    onTabSelected = { screen -> currentScreen = screen },
+                    currentScreen = currentScreen
                 )
             }
         }
+    }
 
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
         composeTestRule
             .onNode(
                 hasText(RallyScreen.Accounts.name.uppercase()) and
                         hasParent(
                             hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
+            .assertExists()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_changeSelectionWhenOtherTabClick() {
+        composeTestRule
+            .onNode(
+                hasContentDescription(RallyScreen.Overview.name)
+                , useUnmergedTree = true)
+            .performClick()
+
+        composeTestRule
+            .onNode(
+                hasText(RallyScreen.Overview.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Overview.name)
                         ),
                 useUnmergedTree = true
             )
