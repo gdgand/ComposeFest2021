78f8b1eaf1e21319921342cb74d68bebc12c8f1d
Wanjuuuuu <makarong5798@gmail.com>
2021-11-28 16:35:43 +0900

    Use ScrollState to enable scroll lazy list and animate scroll using coroutine

--- week 2-1-Layouts in JetpackCompose/LayoutCodelab/app/src/main/java/com/codelab/layoutcodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutCodelab/app/src/main/java/com/codelab/layoutcodelab/MainActivity.kt
@@ -3,22 +3,31 @@ package com.codelab.layoutcodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.codelab.layoutcodelab.ui.theme.LayoutCodelabTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutCodelabTheme {
-                LayoutsCodelab()
+                LazyList()
             }
         }
     }
@@ -55,5 +64,72 @@ fun BodyContent(modifier: Modifier = Modifier) {
 fun LayoutsCodelabPreview() {
     LayoutCodelabTheme {
         LayoutsCodelab()
+    }
+}
+
+@Composable
+fun SimpleList() {
+    val scrollState = rememberScrollState()
+
+    Column(modifier = Modifier.verticalScroll(scrollState)) {
+        repeat(100) {
+            Text("Item $it")
+        }
+    }
+}
+
+@Composable
+fun LazyList() {
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    // this coroutineScope is from getContext
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    // avoid blocking the list rendering while we scroll
+                    scrollState.scrollToItem(0)
+                }
+            }) {
+                Text("to top")
+            }
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.scrollToItem(listSize - 1)
+                }
+            }) {
+                Text("to bottom")
+            }
+        }
+        LazyColumn(state = scrollState) {
+            items(listSize /*list나 array도 가능*/) {
+                ImageListItem(it)
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = "Item $index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Preview
+@Composable
+fun ListPreview() {
+    LayoutCodelabTheme {
+        LazyList()
     }
 }
