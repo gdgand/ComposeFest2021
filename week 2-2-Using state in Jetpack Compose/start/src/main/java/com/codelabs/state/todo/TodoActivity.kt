4bcf87ea6268cf1dad840a7958cbf4a76e80e7a2
Wanjuuuuu <makarong5798@gmail.com>
2021-11-29 20:49:26 +0900

    Use state hoisting using event, update state using viewModel, observe state to display state

--- week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoActivity.kt
+++ week 2-2-Using state in Jetpack Compose/start/src/main/java/com/codelabs/state/todo/TodoActivity.kt
@@ -21,6 +21,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import com.codelabs.state.ui.StateCodelabTheme
 
 class TodoActivity : AppCompatActivity() {
@@ -32,9 +35,18 @@ class TodoActivity : AppCompatActivity() {
         setContent {
             StateCodelabTheme {
                 Surface {
-                    // TODO: build the screen in compose
+                    TodoActivityScreen(todoViewModel = todoViewModel)
                 }
             }
         }
+    }
+
+    @Composable
+    private fun TodoActivityScreen(todoViewModel: TodoViewModel) {
+        val items: List<TodoItem> by todoViewModel.todoItems.observeAsState(initial = listOf())
+        TodoScreen(
+            items = items, onAddItem = todoViewModel::addItem, onRemoveItem =
+            todoViewModel::removeItem
+        )
     }
 }
