ddccc1dc8d3c25b8a6e6eaa7d85fb79b8bcf9d9c
Wanjuuuuu <makarong5798@gmail.com>
2021-12-04 22:53:32 +0900

    Support deeplink for destination accounts/name

--- week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ week 3-3-Jetpack Compose Navigation/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -36,6 +36,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import androidx.navigation.navDeepLink
 import com.example.compose.rally.data.UserData
 import com.example.compose.rally.ui.accounts.AccountsBody
 import com.example.compose.rally.ui.accounts.SingleAccountBody
@@ -105,7 +106,10 @@ fun RallyApp() {
                             // Make argument type safe
                             type = NavType.StringType
                         }
-                    )
+                    ),
+                    deepLinks = listOf(navDeepLink {
+                        uriPattern = "rally://${RallyScreen.Accounts.name}$/{name}"
+                    })
                 ) { entry -> // Look up "name" in NavBackStackEntry's arguments
                     val accountName = entry.arguments?.getString("name")
                     // Find first name match in UserData
