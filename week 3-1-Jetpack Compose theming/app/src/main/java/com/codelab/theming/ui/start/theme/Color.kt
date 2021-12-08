2ace431c10226c5c5a6cd428b9088d70f079ec79
Devinear <leech8757@gmail.com>
2021-12-08 22:20:09 +0900

    week 3-1 Jetpack Compose theming

---
+++ week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.ui.graphics.Color
+
+/**
+ * Color.kt
+ * ComposeTheming
+ */
+
+val Red200 = Color(0xfff297a2)
+val Red300 = Color(0xffea6d7e)
+val Red700 = Color(0xffdd0d3c)
+val Red800 = Color(0xffd00036)
+val Red900 = Color(0xffc20029)
---
+++ week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/theme/Shape.kt
@@ -0,0 +1,17 @@
+package com.codelab.theming.ui.start.theme
+
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+/**
+ * Shape.kt
+ * ComposeTheming
+ */
+
+val JetnewsShapes = Shapes(
+    small = CutCornerShape(topStart = 8.dp),
+    medium = CutCornerShape(topStart = 24.dp),
+    large = RoundedCornerShape(8.dp)
+)
