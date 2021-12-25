136b7494b0ab39a405a15435637ed45edd1c219f
MinSu Kim <ms001118@gmail.com>
2021-12-25 23:57:08 +0900

    week2-1 init

---
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/ms/composelayout/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.ms.composelayout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.ms.composelayout.ui.theme.ComposeLayoutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeLayoutTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeLayoutTheme {
+        Greeting("Android")
+    }
+}
