1fae98598fc6bb52ef838cb6d29c7d9e1e8259d0
MinSu Kim <ms001118@gmail.com>
2021-12-26 00:28:20 +0900

    fix: change modifier attributes of Row

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/ms/composelayout/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/ms/composelayout/MainActivity.kt
@@ -3,17 +3,20 @@ package com.ms.composelayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,7 +36,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun PhotographCard() {
     Row (
-        modifier = Modifier.clickable {  }.padding(16.dp)){
+        modifier = Modifier.padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable(onClick = {})
+            .padding(8.dp)){
         Surface(
             modifier = Modifier.size(50.dp),
             shape = CircleShape,
