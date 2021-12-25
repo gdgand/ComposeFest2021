f422d0464a9560de8766616079b4cdd6dc82ef8d
MinSu Kim <ms001118@gmail.com>
2021-12-25 10:40:09 +0900

    done week1-jetpack-compose-basics

---
+++ week 1-Jetpack Compose basics/ComposeCodelab/app/src/main/java/com/ms/composecodelab/ui/theme/Theme.kt
@@ -0,0 +1,50 @@
+package com.ms.composecodelab.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Navy,
+    onPrimary = Chartreuse,
+    surface = Blue,
+    onSurface = Navy
+)
+
+private val LightColorPalette = lightColors(
+    primary = LightBlue,
+    onPrimary = Navy,
+    surface = Blue,
+    onSurface = Color.White
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun ComposeCodelabTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
