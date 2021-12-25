943f3baf35067c3bbd07732edd53e18fe0c476e2
MinSu Kim <ms001118@gmail.com>
2021-12-26 01:34:51 +0900

    feat: add scroll to the top and bottom buttons
    LazyColumn scroll animation is in coroutinScope.
    Because it allows us to smoothly scroll to the top and bottom

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/ms/composelayout/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/ms/composelayout/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -23,6 +24,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.ms.composelayout.ui.theme.ComposeLayoutTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,10 +49,35 @@ fun SimpleList() {
 
 @Composable
 fun ImageList() {
+    val listSize = 100
     val scrollState = rememberLazyListState()
-    LazyColumn(state = scrollState) {
-        items(100) {
-            ImageListItem(index = it)
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    // 0 is the first item index
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text("Scroll to the top")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    // listSize - 1 is the last index of the list
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text("Scroll to the end")
+            }
+        }
+
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(it)
+            }
         }
     }
 }
