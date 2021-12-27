e3c5dc908ef0389567a461e35c013f50812c4621
MinSu Kim <ms001118@gmail.com>
2021-12-27 17:56:32 +0900

    feat: create @Composable Chip() and apply it

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/ms/composelayout/ComplexCustomLayout.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/ms/composelayout/ComplexCustomLayout.kt
@@ -3,11 +3,23 @@ package com.ms.composelayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.ms.composelayout.ui.theme.ComposeLayoutTheme
 
 class ComplexCustomLayout : ComponentActivity() {
@@ -15,24 +27,100 @@ class ComplexCustomLayout : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeLayoutTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                BodyContent()
             }
+        }
+    }
+}
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
+@Composable
+private fun BodyContent(modifier: Modifier = Modifier) {
+    StaggeredGrid(modifier = modifier) {
+        for (topic in topics) {
+            Chip(modifier = Modifier.padding(8.dp), text = topic)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(modifier = Modifier
+                .size(16.dp, 16.dp)
+                .background(color = MaterialTheme.colors.secondary))
+            Spacer(Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+}
+
+/**
+ * 각 child의 measure는 한번만 실행하기 때문 너비, 높이를 저장해야함
+ * rowWidths[], rowHeights[]에 저장
+ */
+@Composable
+fun StaggeredGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measureables, constraints ->
+        val rowWidths = IntArray(rows) { 0 }
+        val rowHeights = IntArray(rows) { 0 }
+
+        val placeables = measureables.mapIndexed { index, measurable ->
+
+            val placeable = measurable.measure(constraints)
+
+            val row = index % rows
+            rowWidths[row] += placeable.width
+            rowHeights[row] = Math.max(rowHeights[row], placeable.height)
+            placeable
+        }
+
+        val width = rowWidths.maxOrNull()?.coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight)) ?: constraints.minWidth
+        val height = rowHeights.sumOf {it}.coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows) { 0 }
+        for(i in 1 until rows) {
+            rowY[i] = rowY[i - 1] + rowHeights[i - 1]
+        }
+
+        layout(width, height) {
+            val rowX = IntArray(rows) { 0 }
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview2() {
     ComposeLayoutTheme {
-        Greeting("Android")
+        BodyContent()
     }
 }
